7f118ca41b0e1bf37192529e5692718e9dd31dbf
Arte2mkalash19art <129776037+Arte2mkalash19art@users.noreply.github.com>
2024-02-12 14:51:19 +0000

    Deploying to gh-pages from @ Arte2mkalash19art/novabydova@ae97ec28ed6a51f9740811cc73313aa71a6174bc 🚀

---
+++ index.16e3d5fc.js
@@ -0,0 +1,2 @@
+document.querySelector(".burger").addEventListener("click",(function(){this.classList.toggle("active"),document.querySelector(".nav").classList.toggle("open")}));
+//# sourceMappingURL=index.16e3d5fc.js.map
