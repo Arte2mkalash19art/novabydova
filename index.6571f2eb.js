9df464c007dc1e78dc56fb907a2bc45d62ed6f9d
Arte2mkalash19art <129776037+Arte2mkalash19art@users.noreply.github.com>
2024-02-17 17:45:51 +0000

    Deploying to gh-pages from @ Arte2mkalash19art/novabydova@be4ba3740c81f25c530152765cf51d6bcf65e7ba 🚀

---
+++ index.6571f2eb.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-backdrop]")};function e(){o.modal.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.6571f2eb.js.map
