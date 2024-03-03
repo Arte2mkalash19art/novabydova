31fa770d7b99f4f5931bdc8433627d4c67bd7752
Arte2mkalash19art <129776037+Arte2mkalash19art@users.noreply.github.com>
2024-03-03 10:00:14 +0000

    Deploying to gh-pages from @ Arte2mkalash19art/novabydova@20d12ae15b289b6a5eb489707dae8fe956221898 🚀

---
+++ index.9ca36d59.js
@@ -0,0 +1,2 @@
+function scrollToTop(){window.scrollTo({top:0,behavior:"smooth"})}function scrollFunction(){document.body.scrollTop>800||document.documentElement.scrollTop>800?document.getElementById("scrollBtn").style.display="block":document.getElementById("scrollBtn").style.display="none"}window.onscroll=function(){scrollFunction()};
+//# sourceMappingURL=index.9ca36d59.js.map
